475e51b9568754891ac0be2651030cb16e962d3a
adityaagg09 <adityaaggarwal092@gmail.com>
2023-04-15 03:21:42 +0530

    Day11: Adding unit test cases

---
+++ src/test/java/Base/LandingPage/Scheduler/SchedulerTest.java
@@ -0,0 +1,24 @@
+package Base.LandingPage.Scheduler;
+
+import Base.LandingPage.Enums.StatesEnumMapping;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class SchedulerTest {
+
+    @InjectMocks
+    Scheduler scheduler;
+
+    @Test
+    public void test_schedulerCompleteData() {
+        scheduler.scheduler();
+    }
+
+}
