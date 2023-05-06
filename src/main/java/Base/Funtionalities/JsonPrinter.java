b3174cf698dc29cebe403f6ce536d88b71a38eed
adityaagg09 <adityaaggarwal092@gmail.com>
2023-05-06 18:29:54 +0530

    Day 15: Json Print statements

---
+++ src/main/java/Base/Funtionalities/JsonPrinter.java
@@ -0,0 +1,36 @@
+package Base.Funtionalities;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.gson.GsonBuilderCustomizer;
+
+public class JsonPrinter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonPrinter.class);
+
+    public static String toJson(Object obj) {
+        String json = StringUtils.EMPTY;
+        try {
+            Gson gson =  new GsonBuilder().setPrettyPrinting().create();
+            json = gson.toJson(obj);
+        } catch (Exception ex) {
+            LOGGER.error("Please provide correct data format");
+        }
+        return  json;
+    }
+
+    public static String toJsonText(Object obj) {
+        String json = StringUtils.EMPTY;
+        try {
+            Gson gson =  new GsonBuilder().create();
+            json = gson.toJson(obj);
+        } catch (Exception ex) {
+            LOGGER.error("Please provide correct data format");
+        }
+        return  json;
+    }
+
+}
