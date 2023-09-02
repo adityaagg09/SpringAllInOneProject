b3154c02a9516c7e0fa276300dfb7a1a7298f0d0
adityaagg09 <adityaaggarwal092@gmail.com>
2023-09-02 14:50:03 +0530

    Day28: Generating url from a map

--- src/main/java/Base/LandingPage/API/CallingCode.java
+++ src/main/java/Base/LandingPage/API/CallingCode.java
@@ -5,6 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class CallingCode {
@@ -17,6 +21,15 @@ public class CallingCode {
     @Transactional // If transaction is not completed it will revoked all the db inserts
     public String getDataFromApi() {
         String url = "https://jsonplaceholder.typicode.com/todos/1";
+
+
+        // Creating url with map received from ui for different url params
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("Test", "Test2");
+        map.add("Test", "Test3");
+        map.add("Test1", "Test2");
+        UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(url).queryParams(map).build();
+        String urlFromUriComponents = uriComponents.toUriString();
 
         // Initiating GET request
         String getRequestResult = String.valueOf(restApi.GET(String.class, url).getBody());
